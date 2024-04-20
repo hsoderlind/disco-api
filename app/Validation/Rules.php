70ab7acb6ca1bef846236702ba2a08b44defcdd8
hsoderlind <henrik.soderlind@live.se>
2024-04-20 23:28:55 +0200

    added a method to Rules that prepend sometimes to a set of rules

--- app/Validation/Rules.php
+++ app/Validation/Rules.php
@@ -4,6 +4,8 @@ namespace App\Validation;
 
 use App\Interfaces\IRules;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class Rules implements IRules
 {
@@ -25,5 +27,31 @@ class Rules implements IRules
     public function getRules(): array
     {
         return [];
+    }
+
+    public function sometimes(array $rules): array
+    {
+        $newRules = [];
+
+        foreach ($rules as $key => $value) {
+            $newRules[$key] = $this->prependSometimes($value);
+        }
+
+        return $newRules;
+    }
+
+    private function prependSometimes(array|string $conditions): array|string
+    {
+        if (is_array($conditions) && ! in_array('sometimes', $conditions)) {
+            $conditions = Arr::prepend($conditions, 'sometimes');
+
+            return $conditions;
+        }
+
+        if (! Str::contains($conditions, 'sometimes')) {
+            $conditions = 'sometimes|'.$conditions;
+
+            return $conditions;
+        }
     }
 }
