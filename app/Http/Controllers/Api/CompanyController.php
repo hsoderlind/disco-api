73f4c6edb3b5642a3272bbf64525df159c96ce3a
hsoderlind <henrik.soderlind@live.se>
2024-05-22 21:48:25 +0200

    moved controllers to correct directory

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\CompanyRequest;
 use App\Services\Company\CompanyService;
 use App\Services\Shop\ShopSession;
--- app/Http/Controllers/Api/InvoiceSettingsController.php
+++ app/Http/Controllers/Api/InvoiceSettingsController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\InvoiceSettingsRequest;
 use App\Http\Requests\LogotypeRequest;
 use App\Http\Resources\InvoiceSettingsResource;
