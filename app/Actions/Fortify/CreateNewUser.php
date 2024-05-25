f1c5a0c5cf056b53eef938778f34d5d00519ae8c
hsoderlind <henrik.soderlind@live.se>
2024-05-25 18:49:48 +0200

    changing in how a user is created depending on if the user has been invited to a shop account.

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
@@ -19,6 +20,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+        // 1. Validate input
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
@@ -26,15 +28,35 @@ class CreateNewUser implements CreatesNewUsers
                 'string',
                 'email',
                 'max:255',
-                Rule::unique(User::class),
+                // Rule::unique(User::class),
             ],
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+        // 2. Check if any user with given email exists
+        /** @var \App\Models\User|null $user */
+        $user = User::where('email', $input['email'])->first();
+        if (! is_null($user) && $user->state === 'REGISTERED') {
+            throw ValidationException::withMessages([
+                'email' => ['Felaktig e-postadress'],
+            ]);
+        }
+
+        if (! is_null($user) && $user->state === 'INVITED') {
+            $user->update([
+                'name' => $input['name'],
+                'password' => Hash::make($input['password']),
+                'state' => 'REGISTERED',
+            ]);
+
+            return $user;
+        } elseif (is_null($user)) {
+            return User::create([
+                'name' => $input['name'],
+                'email' => $input['email'],
+                'password' => Hash::make($input['password']),
+            ]);
+        }
+
     }
 }
