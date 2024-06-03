baac0eec521c79ece001f3493a8a8d163fa4c570
hsoderlind <henrik.soderlind@live.se>
2024-06-03 19:18:36 +0200

    Rule to check if attribute is unique in shop

---
+++ app/Rules/UniqueInShop.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Rules;
+
+use App\Services\Shop\ShopSession;
+use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\DB;
+
+class UniqueInShop implements DataAwareRule, ValidationRule
+{
+    protected $column = 'shop_id';
+
+    protected int $shopId;
+
+    protected bool $ignore = false;
+
+    protected ?string $except = null;
+
+    protected array $data = [];
+
+    public function __construct(
+        protected readonly string $table,
+        ?string $column = null,
+        ?int $shopId = null,
+        ?string $except = null
+    ) {
+        $this->column = $column ?? $this->column;
+        $this->shopId = $shopId ?? ShopSession::getId();
+        $this->except = $except;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $query = DB::table($this->table)->where($attribute, $value)->where($this->column, $this->shopId);
+
+        if (! is_null($this->except) && array_key_exists($this->except, $this->data)) {
+            $query->where($this->except, '!=', $this->data[$this->except]);
+        }
+
+        $exists = $query->exists();
+
+        if (! $this->ignore && $exists) {
+            $fail('unique')->translate();
+        }
+    }
+
+    public function ignore(bool $condition)
+    {
+        $this->ignore = $condition;
+
+        return $this;
+    }
+
+    public function setData(array $data)
+    {
+        $this->data = $data;
+    }
+}
--- app/Services/OrderStatus/OrderStatusRules.php
+++ app/Services/OrderStatus/OrderStatusRules.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\OrderStatus;
 
+use App\Rules\UniqueInShop;
 use App\Validation\Rules;
 
 class OrderStatusRules extends Rules
@@ -20,7 +21,7 @@ class OrderStatusRules extends Rules
     {
         return [
             'id' => 'sometimes|required|exists:order_statuses,id',
-            'name' => 'required|string|max:255',
+            'name' => ['required', 'string', 'max:255', new UniqueInShop('order_statuses', except: 'id')],
             'sort_order' => 'required|numeric|integer|min:0',
             'is_default' => 'required|boolean',
         ];
